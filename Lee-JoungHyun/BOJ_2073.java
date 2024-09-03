5f57756dbd92968efd874293a02dd032f39b124e
Lee-JH <98592001+Lee-JoungHyun@users.noreply.github.com>
2024-09-04 02:36:12 +0900

    수도배관공사 / 50분

---
+++ Lee-JoungHyun/BOJ_2073.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2073 {
+
+    static int d; 
+    static int p; 
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        d = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
+
+        dp = new int[d + 1];
+        dp[0] = Integer.MAX_VALUE;
+
+        while (p-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken()); 
+
+            for (int i = d; i >= l; i--) {
+                dp[i] = Math.max(dp[i], Math.min(dp[i-l], c));
+            }
+        }
+
+        System.out.println(dp[d]);
+    }
+}
